1ac9463949f9491067aa3fe44f103722e20e9cb0
hainet <20400616+hainet@users.noreply.github.com>
2018-02-28 23:35:13 +0900

    ArgumentMatcherの利用サンプルを追加

--- src/test/java/MathServiceTest.java
+++ src/test/java/MathServiceTest.java
@@ -1,4 +1,5 @@
 import org.junit.Test;
+import org.mockito.exceptions.misusing.InvalidUseOfMatchersException;
 
 import static org.mockito.Mockito.*;
 
@@ -8,28 +9,80 @@ import static org.hamcrest.CoreMatchers.is;
 public class MathServiceTest {
 
     @Test
-    // モックインスタンスの対象メソッドが適切な引数で呼ばれているか確認する。
+    // モックインスタンスの対象メソッドが適切な引数で呼ばれていることを確認する。
     public void verifyTest() {
+        // Setup
         final MathService mathService = mock(MathService.class);
 
+        // Exercise
+        // sumとsubtractがそれぞれ引数1で呼ばれていることを確認したい。
         mathService.sum(1);
         mathService.subtract(1);
 
+        // Verify
         verify(mathService).sum(1);
         verify(mathService).subtract(1);
     }
 
     @Test
-    // モックインスタンスの対象メソッドが適切な引数で適切な回数呼ばれているか確認する。
+    // モックインスタンスの対象メソッドが適切な引数で適切な回数呼ばれていることを確認する。
     public void verifyTimesTest() {
+        // Setup
         final MathService mathService = mock(MathService.class);
 
+        // Exercise
+        // sumが引数1で1回、引数2で2回呼ばれていることを確認したい。
         mathService.sum(1);
         mathService.sum(2);
         mathService.sum(2);
 
+        // Verify
         verify(mathService, times(1)).sum(1);
         verify(mathService, times(2)).sum(2);
+    }
+
+    @Test
+    // 引数に関わらず適切な回数呼ばれていることを確認する。
+    public void verifyAnyIntTest() {
+        // Setup
+        final MathService mathService = mock(MathService.class);
+
+        // Exercise
+        // sumが引数1から100まで、それぞれ1回ずつ合計100回呼ばれていることを確認したい。
+        for (int i=1; i<=100; i++) {
+            mathService.sum(i);
+        }
+
+        // Verify
+        verify(mathService, times(100)).sum(anyInt());
+    }
+
+    @Test
+    // 複数の引数を持つメソッドにMatcherを適用する。
+    public void correctArgumentMatcherTest() {
+        // Setup
+        final PlusComponent plusComponent = mock(PlusComponent.class);
+
+        // Exercise
+        plusComponent.run(1, 2);
+
+        // Verify
+        // 複数のMatcherを利用できることを確認したい。
+        verify(plusComponent).run(anyInt(), anyInt());
+    }
+
+    @Test(expected = InvalidUseOfMatchersException.class)
+    // 複数の引数を持つメソッドにMatcherと値を同時に適用する。
+    public void incorrectArgumentMatcherTest() {
+        // Setup
+        final PlusComponent plusComponent = mock(PlusComponent.class);
+
+        // Exercise
+        plusComponent.run(1, 2);
+
+        // Verify
+        // Matcherと値が併用できないことを確認したい。
+        verify(plusComponent).run(anyInt(), 2);
     }
 
     @Test
@@ -40,7 +93,9 @@ public class MathServiceTest {
         when(mathService.sum(1)).thenReturn(1);
 
         // Exercise & Verify
+        // sumに設定した値が返却されることを確認したい。
         assertThat(mathService.sum(1), is(1));
+        // sumに設定していない値は0で返却されることを確認したい。
         assertThat(mathService.sum(999), is(0));
     }
 
@@ -52,6 +107,20 @@ public class MathServiceTest {
         when(mathService.result()).thenThrow(new RuntimeException());
 
         // Exercise
+        // resultに設定した例外が発生することを確認したい。
         mathService.result();
+    }
+
+    @Test
+    public void whenAnyIntTest() {
+        // Setup
+        final MathService mathService = mock(MathService.class);
+        when(mathService.sum(anyInt())).thenReturn(999);
+
+        // Exercise & Verify
+        // sumの引数に関わらず、設定した値が返却されることを確認したい。
+        assertThat(mathService.sum(1), is(999));
+        assertThat(mathService.sum(2), is(999));
+        assertThat(mathService.sum(3), is(999));
     }
 }
